4e7ba99ce11cee7dbdc35e8a5d0935b1fdbf5265
kimnamheeee <nami200284@snu.ac.kr>
2025-10-01 14:04:26 +0900

    ✨ feat(components): add navigation component

---
+++ src/components/Navigation.tsx
@@ -0,0 +1,7 @@
+export default function Navigation() {
+  return (
+    <nav className="flex items-center justify-center px-2 py-4 w-full h-20 text-2xl font-bold shadow-[0_4px_6px_rgba(0,0,0,0.1)]">
+      <h1>천개의 레시피</h1>
+    </nav>
+  );
+}
