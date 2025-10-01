2ed42421dbfad85a67ae59e426b66130270793be
kimnamheeee <nami200284@snu.ac.kr>
2025-10-01 16:06:41 +0900

    ✨ feat(components): add Spinner component

---
+++ src/components/Spinner.tsx
@@ -0,0 +1,7 @@
+export default function Spinner() {
+  return (
+    <div className="flex items-center justify-center w-full h-full">
+      <div className="w-8 h-8 border-4 border-gray-200 border-t-gray-900 rounded-full animate-spin" />
+    </div>
+  );
+}
