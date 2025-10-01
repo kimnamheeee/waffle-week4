85a86d87dcc8e06d3b6d62b72daa67ade00ae3ab
kimnamheeee <nami200284@snu.ac.kr>
2025-10-01 15:42:44 +0900

    💎 style(recipecard): change img style

--- src/components/RecipeCard.tsx
+++ src/components/RecipeCard.tsx
@@ -15,8 +15,8 @@ export default function RecipeCard({
 }: RecipeCardProps) {
   return (
     <div className="w-[330px] h-[270px] flex flex-col rounded-2xl overflow-hidden">
-      <img src={src} alt={title} className="w-full h-full object-cover " />
-      <div className="flex flex-col bg-white gap-4 p-4 h-auto">
+      <img src={src} alt={title} className="object-cover overflow-hidden" />
+      <div className="flex flex-col bg-white gap-4 p-4">
         <div className="flex justify-between items-center">
           <h1 className="text-xl font-semibold">{title}</h1>
           <p className="text-sm text-accent-foreground">{difficulty}</p>
