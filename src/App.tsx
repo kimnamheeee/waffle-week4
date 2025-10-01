f76a5f854576850397246a256a9e4e324425f645
kimnamheeee <nami200284@snu.ac.kr>
2025-10-01 18:21:23 +0900

    ✨ feat(app.tsx): apply router

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,23 @@
 import { Suspense } from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Navigation from './components/Navigation';
 import Spinner from './components/Spinner';
+import DetailPage from './pages/DetailPage';
 import MainPage from './pages/MainPage';
 
 const App = () => {
   return (
-    <div className="w-full h-full">
-      <Navigation />
-      <Suspense fallback={<Spinner />}>
-        <MainPage />
-      </Suspense>
-    </div>
+    <BrowserRouter>
+      <div className="w-full h-full">
+        <Navigation />
+        <Suspense fallback={<Spinner />}>
+          <Routes>
+            <Route path="/" element={<MainPage />} />
+            <Route path="/recipe/:id" element={<DetailPage />} />
+          </Routes>
+        </Suspense>
+      </div>
+    </BrowserRouter>
   );
 };
 
